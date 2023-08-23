6252b08e55de5d9152099715e66f368e93767aa6
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-08-23 15:13:42 +0530

    updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -94,7 +94,7 @@ const Projects = () => {
               <p><LeftLink href="https://cartapp-redux.netlify.app/"  target="_blank">3. Shopping App using Redux</LeftLink></p>
               <p><LeftLink href="https://todo-app-reducer-shwetank.netlify.app/"  target="_blank">4. Todo App using useReducer </LeftLink></p>
               <p><LeftLink href="https://memegen-shwetank.netlify.app/" target="_blank"> 5. Meme Generator</LeftLink></p>  
-              <p><LeftLink href='https://github.com/shwetank00001/dashboard_app' target="_blank"> 6. Dashboard App with multiple functions</LeftLink></p> 
+              <p><LeftLink href='https://shwetank-main-dashboard-app.netlify.app' target="_blank"> 6. Dashboard App with Multiple Sections</LeftLink></p> 
           </div>
           <div>
             
